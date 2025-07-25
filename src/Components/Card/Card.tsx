1f07eba3aa29f1b5fee951a8748b65e9e14311a7
alenko998 <alen_git@hotmail.com>
2025-07-25 17:28:51 +0200

    Function Components

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -7,7 +7,7 @@ interface Props {
     price: number;
 }
 
-const Card = ({companyName,ticker,price}: Props) => {
+const Card: React.FC<Props> = ({companyName,ticker,price}: Props) : JSX.Element => {
   return <div className="card">
     <img src="https://images.unsplash.com/photo-1682905926517-6be3768e29f0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image" />
     <div className="details">
