26d55c6103b51880c2636b95278f4894efafc5dc
alenko998 <alen_git@hotmail.com>
2025-07-25 17:22:17 +0200

    Props

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import "./Card.css"
 
-type Props = {}
+interface Props {
+    companyName: string;
+    ticker: string;
+    price: number;
+}
 
-const Card = (props: Props) => {
+const Card = ({companyName,ticker,price}: Props) => {
   return <div className="card">
     <img src="https://images.unsplash.com/photo-1682905926517-6be3768e29f0?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image" />
     <div className="details">
-        <h2>AAPL</h2>
-        <p>$110</p>
+        <h2>{companyName} ({ticker})</h2>
+        <p>${price}</p>
     </div>
     <p className='info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, laudantium?</p>
   </div>
